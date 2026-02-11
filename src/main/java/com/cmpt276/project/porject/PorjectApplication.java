c5c0e01ecc2a41077eecd2296491c60225620b2c
Ali Alsaadiq <aalsaadiq@my.bcit.ca>
2026-02-10 22:15:34 -0800

    first commit

---
+++ src/main/java/com/cmpt276/project/porject/PorjectApplication.java
@@ -0,0 +1,13 @@
+package com.cmpt276.project.porject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PorjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PorjectApplication.class, args);
+	}
+
+}
