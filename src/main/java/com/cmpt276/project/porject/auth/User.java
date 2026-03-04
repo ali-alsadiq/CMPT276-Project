bea464627f9f4d982576c01178795e3e54198d58
Jonathan Corpuz <jonocorpuz@gmail.com>
2026-03-04 15:16:03 -0800

    feature: design change

---
+++ src/main/java/com/cmpt276/project/porject/auth/User.java
@@ -0,0 +1,59 @@
+package com.cmpt276.project.porject.auth;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int uid;
+
+    private String username;
+    private String password;
+    private String role;
+
+    public User() {
+
+    }
+
+    public User(String username, String password, String role) {
+        this.username = username;
+        this.password = password;
+        this.role = role;
+    }
+
+    public int getUid() {
+        return uid;
+    }
+
+    public void setUid(int uid) {
+        this.uid = uid;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+}
