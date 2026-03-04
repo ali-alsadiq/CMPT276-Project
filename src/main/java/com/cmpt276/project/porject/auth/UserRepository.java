224af6aee91ee99076412b7279ba049f36c25817
Jonathan Corpuz <jonocorpuz@gmail.com>
2026-03-03 18:02:59 -0800

    feat: implement UserRepository.java

---
+++ src/main/java/com/cmpt276/project/porject/auth/UserRepository.java
@@ -0,0 +1,12 @@
+package com.cmpt276.project.porject.auth;
+
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+    List<User> findAll();
+
+    User findByUsernameAndPassword(String username, String password);
+
+    User findByUid(int uid);
+}
