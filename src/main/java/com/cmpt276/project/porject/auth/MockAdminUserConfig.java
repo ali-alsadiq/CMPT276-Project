4f72a336a784c3d35916ad362eb081846ec76ccd
Jonathan Corpuz <jonocorpuz@gmail.com>
2026-03-07 10:33:59 -0800

    feat: add redirect to admin dashboard

---
+++ src/main/java/com/cmpt276/project/porject/auth/MockAdminUserConfig.java
@@ -0,0 +1,26 @@
+package com.cmpt276.project.porject.auth;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Configuration to add a mock admin user to the database on startup.
+ * Easily removable before deploying to production.
+ */
+@Configuration
+public class MockAdminUserConfig {
+
+    @Bean
+    public CommandLineRunner initMockAdminUser(UserRepository userRepository) {
+        return args -> {
+            try {
+                userRepository.save(new User("System", "Admin", "admin", "admin", "ADMIN"));
+            }
+
+            catch (Exception e) {
+                // Ignore if it already exists
+            }
+        };
+    }
+}
