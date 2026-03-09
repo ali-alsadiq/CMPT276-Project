296374ce8c886597ace13e4d125e5aada2fa0fb7
VotableSFU57 <cjj7@sfu.ca>
2026-03-08 18:38:37 -0700

    Readded food getters/setters

--- src/main/java/com/cmpt276/project/porject/trackers/nutrition/Food.java
+++ src/main/java/com/cmpt276/project/porject/trackers/nutrition/Food.java
@@ -73,6 +73,122 @@ public class Food {
 
     }
 
+	public int getId() {
+		return id;
+	}
 
+	public String getFoodName() {
+		return foodName;
+	}
+
+	public void setFoodName(String foodName) {
+		this.foodName = foodName;
+	}
+
+	public int getUserId() {
+		return userId;
+	}
+
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+
+	public double getCalories() {
+		return calories;
+	}
+
+	public void setCalories(double calories) {
+		this.calories = calories;
+	}
+
+	public double getServSize() {
+		return servSize;
+	}
+
+	public void setServSize(double servSize) {
+		this.servSize = servSize;
+	}
+
+	public double getProtien() {
+		return protien;
+	}
+
+	public void setProtien(double protien) {
+		this.protien = protien;
+	}
+
+	public double getCarbs() {
+		return carbs;
+	}
+
+	public void setCarbs(double carbs) {
+		this.carbs = carbs;
+	}
+
+	public double getFats() {
+		return fats;
+	}
+
+	public void setFats(double fats) {
+		this.fats = fats;
+	}
+
+	public double getFiber() {
+		return fiber;
+	}
+
+	public void setFiber(double fiber) {
+		this.fiber = fiber;
+	}
+
+	public double getSugar() {
+		return sugar;
+	}
+
+	public void setSugar(double sugar) {
+		this.sugar = sugar;
+	}
+
+	public double getSodium() {
+		return sodium;
+	}
+
+	public void setSodium(double sodium) {
+		this.sodium = sodium;
+	}
+
+	public double getPotassium() {
+		return potassium;
+	}
+
+	public void setPotassium(double potassium) {
+		this.potassium = potassium;
+	}
+
+	public double getCholesterol() {
+		return cholesterol;
+	}
+
+	public void setCholesterol(double cholesterol) {
+		this.cholesterol = cholesterol;
+	}
+
+	public LocalDateTime getConsumedAt() {
+		return consumedAt;
+	}
+
+	public void setConsumedAt(LocalDateTime consumedAt) {
+		this.consumedAt = consumedAt;
+	}
+
+	public LocalDateTime getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(LocalDateTime createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	
     
 }
