8c23f209792f003b32ebd7cea260f2a35c40a3c4
Jonathan Corpuz <jonocorpuz@gmail.com>
2026-03-18 12:57:06 -0700

    feat: navbar stays expanded after new tab

---
+++ src/main/resources/static/js/navbar.js
@@ -0,0 +1,40 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const sidebar = document.getElementById('dynamic-sidebar');
+    const delayInMilliseconds = 500; 
+    let expandTimer; 
+
+    if (sidebar) {
+
+        // Check if the navbar was expanded before changing tabs
+        if (sessionStorage.getItem('sidebarWasOpen') === 'true') {
+            // Force it open instantly without waiting for the hover timer
+            sidebar.classList.add('is-expanded');
+            
+            sessionStorage.removeItem('sidebarWasOpen'); // Clear the temp memory
+        }
+
+        // Saving the state on click
+        // - Allows for the navbar to stay expanded after changing tab
+        const navLinks = sidebar.querySelectorAll('a');
+        navLinks.forEach(link => {
+            link.addEventListener('click', () => {
+                if (sidebar.classList.contains('is-expanded')) {
+                    // Save state temporarily in memory
+                    sessionStorage.setItem('sidebarWasOpen', 'true');
+                }
+            });
+        });
+
+        // Mouse hover to expand
+        sidebar.addEventListener('mouseenter', () => {
+            expandTimer = setTimeout(() => {
+                sidebar.classList.add('is-expanded');
+            }, delayInMilliseconds); 
+        });
+
+        sidebar.addEventListener('mouseleave', () => {
+            clearTimeout(expandTimer);
+            sidebar.classList.remove('is-expanded');
+        });
+    }
+});
